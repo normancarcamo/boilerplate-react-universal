f16fd71dc3c73e28dfc362de53444993dc54635d
Norman Carcamo <normancarcamo@gmail.com>
2017-10-07 18:04:00 -0600

    added mover third party libraries to the vendor bundle

--- config/webpack/shared/vendors.js
+++ config/webpack/shared/vendors.js
@@ -1,3 +1,11 @@
 module.exports = [
-  'superagent', 'moment', 'immutable'
+  'superagent',
+  'moment',
+  'immutable',
+  'react',
+  'react-dom',
+  'react-router',
+  'prop-types',
+  'react-router-config',
+  'react-router-dom'
 ];
